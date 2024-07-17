8d33dd81ed73b89075b4536ffec599aca36eb846
yaawar kapadia <iqbalkapadia40@gmail.com>
2024-07-17 13:53:54 +0530

    Home Page Added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import { ToastHeader } from "react-bootstrap";
 
 const Home = () => {
+  function App() {
+    const navigateToLogin = () => {
+      window.location.href = '/login';
+    };
+  }
   return (
     <div>
-      Home
-      <Link to="/login">Go to Login</Link>
+     <header class="header">
+        <a href="#" class="logo">Routify</a>
+
+        <nav class="navbar">
+            <a href="#" class="active">About</a>
+            <a href="#">Notification</a>
+            <a href="#">Profile</a>
+        </nav>
+    </header>
+     <section className="home">
+      <div className="content">
+        <i><h1>YOUR JOURNEY, OUR EXPERTISE</h1></i>
+        <a href="/login">Get Started</a>
+        
+      </div>
+     </section>
     </div>
   );
 };
 
 export default Home;
+
